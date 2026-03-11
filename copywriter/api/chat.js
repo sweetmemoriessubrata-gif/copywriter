9a483ee8ef6528e58a00c4bfbacbb5eec42a3871
sweetmemoriessubrata-gif <sweetmemoriessubrata@gmail.com>
2026-03-11 15:19:43 +0800

    Update chat.js

--- copywriter/api/chat.js
+++ copywriter/api/chat.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const response = await fetch('https://api.anthropic.com/v1/messages', {
+    const response = await fetch('https://new.lemonapi.site/v1/messages', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
